52c5f7042772409b3e44fc13b1661b8e06e8a9b9
Your Name <you@example.com>
2017-06-24 04:36:35 +0800

    Reconfigure

--- format.sh
+++ format.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+function formatSinglefile {
+	uncrustify -c uncrustify_c.cfg  -f $1> $1.format
+	if [ $? -ne 0 ]; then
+		rm -f ${1}.format
+		echo -e "\x1B[1;31mFormat file $1 failed.\x1B[m"
+	else
+		mv -f $1.format $1
+	fi
+}
 
-find . -regex '.*/.*\.\(c\|cpp\|h\)$'  |
-while read filename
-do
-	uncrustify -c uncrustify_c.cfg  -f ${filename}> ${filename}.format
-	mv -f ${filename}.format ${filename}
-done
+function formatDirFiles {
+	find $1 -regex '.*/.*\.\(c\|cpp\|h\)$'  |
+	while read filename
+	do
+		formatSinglefile $filename
+	done
+}
+
+
+
+if [ $# = 1 ]; then
+	if [ -f "$1" ]; then
+		formatSingleFile "$1"
+	elif [ -d "$1" ]; then
+		echo "aaa"
+		formatDirFiles "$1"
+	fi
+else
+	formatDirFiles .
+fi
